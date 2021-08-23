707e5fe103eae61e5778219fe00ab7e5fd96762c
polybittar <polyana.bittar@zup.com.br>
2021-08-23 15:20:50 -0300

    1 commit

---
+++ src/main/java/br/com/zup/polyana/propostas/PropostasApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.polyana.propostas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropostasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropostasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/polyana/propostas/PropostasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.polyana.propostas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
