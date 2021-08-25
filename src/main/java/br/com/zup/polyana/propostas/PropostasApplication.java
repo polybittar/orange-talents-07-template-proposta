511af4ad64ad7ea6d4f0be3dc1698be53f625165
polybittar <polyana.bittar@zup.com.br>
2021-08-25 12:18:48 -0300

    implementação do health check

--- src/main/java/br/com/zup/polyana/propostas/PropostasApplication.java
+++ src/main/java/br/com/zup/polyana/propostas/PropostasApplication.java
@@ -2,10 +2,11 @@ package br.com.zup.polyana.propostas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
-@SpringBootApplication
+@SpringBootApplication(exclude ={SecurityAutoConfiguration.class})
 public class PropostasApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/zup/polyana/propostas/config/MeuHealthCheck.java
+++ src/main/java/br/com/zup/polyana/propostas/config/MeuHealthCheck.java
@@ -14,8 +14,8 @@ public class MeuHealthCheck implements HealthIndicator {
     @Override
     public Health health() {
         Map<String, Object> details = new HashMap<>();
-        details.put("versão", "1.2.3");
-        details.put("descrição", "Meu primeiro Health Check customizado!");
+        details.put("versão", "1.0");
+        details.put("descrição", "Health Check Propostas");
         details.put("endereço", "127.0.0.1");
 
         return Health.status(Status.UP).withDetails(details).build();
